35c01b3629bb6a5795c719dfc93aa69a745e50e4
Duc Tai Nguyen <duc.nguyen@student.tugraz.at>
2024-09-26 09:51:10 +0200

    crop with coordinate

--- code/main.py
+++ code/main.py
@@ -4,34 +4,35 @@ import cv2
 
 model = YOLO("yolov8s.pt")
 
-data_path = "Pose-Estimation-ToF/recording-tof-20221024-0918/kinect/color/0/"
+color_data_path = "Pose-Estimation-ToF/recording-tof-20221024-0918/kinect/color/0/"
+depth_data_path = "Pose-Estimation-ToF/recording-tof-20221024-0918/kinect/depth/0/"
 
 save_dir = "Pose-Estimation-ToF/recording-tof-20221024-0918/cropped/color/"
 
 os.makedirs(save_dir, exist_ok=True)
 
-files = os.listdir(data_path)
+files = os.listdir(color_data_path)
 
-for file in files:
-    file_path = os.path.join(data_path, file)
-
-    if file.endswith(('.jpg', '.jpeg', '.png')):
-        img = cv2.imread(file_path)
-        result = model(source=file_path, show=False, conf=0.4, save=True, save_crop=True, project=save_dir, name="0",
-                       exist_ok=True)
-
-# -------------DIFFERENT VERSION OF CROPPING------------------
 # for file in files:
-#     file_path = os.path.join(data_path, file)
+#     file_path = os.path.join(color_data_path, file)
 #
 #     if file.endswith(('.jpg', '.jpeg', '.png')):
 #         img = cv2.imread(file_path)
-#         result = model(source=file_path, show=False, conf=0.4)
-#
-#         for i, det in enumerate(result[0].boxes.xyxy):
-#             x1, y1, x2, y2 = map(int, det[:4])
-#             crop_img = img[y1:y2, x1:x2]
-#
-#             # save cropped image
-#             crop_path = os.path.join(save_dir, file)
-#             cv2.imwrite(crop_path, crop_img)
+#         result = model(source=file_path, show=False, conf=0.4, save=True, save_crop=True, project=save_dir, name="0",
+#                        exist_ok=True)
+
+# -------------DIFFERENT VERSION OF CROPPING------------------
+for file in files:
+    file_path = os.path.join(color_data_path, file)
+
+    if file.endswith(('.jpg', '.jpeg', '.png')):
+        img = cv2.imread(file_path)
+        result = model(source=file_path, show=False, conf=0.4)
+
+        for i, det in enumerate(result[0].boxes.xyxy):
+            x1, y1, x2, y2 = map(int, det[:4])
+            crop_img = img[y1:y2, x1:x2]
+
+            # save cropped image
+            crop_path = os.path.join(save_dir, file)
+            cv2.imwrite(crop_path, crop_img)
